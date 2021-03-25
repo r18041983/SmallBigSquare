50e83b558a4c80c3c4dbed7ddfcb42f02d9725bc
Rodion Molchanov <r18041983@gmail.com>
2021-03-25 15:47:33 +0700

    Initial commit

--- SmallBigSquare/ViewController.swift
+++ SmallBigSquare/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let squareSize : CGFloat = 30.0
+    let squareOrigin = CGPoint(x: 10, y: 10)
+    let multiplier : CGFloat = 2.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let square = UIView(frame: CGRect(x: self.squareOrigin.x, y: self.squareOrigin.y, width: self.squareSize, height: self.squareSize))
+        
+        square.backgroundColor = UIColor.systemGreen
+        square.center = self.view.center
+        
+        self.view.addSubview(square)
+        
+        UIView.animate(withDuration: 2,
+                       delay: 3,
+                       options: .curveLinear,
+                       animations: { [self] in
+                        square.frame.origin = CGPoint(x: square.center.x - (self.squareSize / 2) * self.multiplier, y: square.center.y - (self.squareSize / 2) * self.multiplier)
+                        square.frame.size = CGSize(width: self.squareSize * self.multiplier, height: self.squareSize * self.multiplier)
+                       },
+                       completion: nil)
     }
-
 
 }
 
